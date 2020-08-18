d124bdebe5f9e8379dea12084d5f71928820e1e7
김근비 <rmsql11@naver.com>
2020-08-18 14:59:09 +0900

    supervisorName(getText로 drop-down list에 가져온 값)으로 인하여 UserEventType.CREATE시에 에러 발생하여 주석처리를 통해 해결!!

--- FileCabinet/SuiteScripts/sdr_ue_employee.js
+++ FileCabinet/SuiteScripts/sdr_ue_employee.js
@@ -5,7 +5,6 @@
 
 define(['N/record'],
     /**
-     *     ['N/module'] varName
      * @param {record} record
      *
      */
@@ -17,29 +16,28 @@ define(['N/record'],
 
                 var employee=context.newRecord;
                 var empCode=employee.getValue('custentity_sdr_employee_code');
-                var supervisorName=employee.getText('supervisor');
+                //var supervisorName=employee.getText('supervisor');
                 var supervisorId=employee.getValue('supervisor');
 
                 //employee.setValue('custentity_sdr_employee_code','EMP002');
 
                 log.debug('Employee Code',empCode);
                 log.debug('Supervisor ID',supervisorId);
-                log.debug('Supervisior Name',supervisorName);
+                //log.debug('Supervisior Name',supervisorName);
 
-                //filtering that the user event that is triggered when creating a new record.
-                if(context.type==context.UserEventType.CREATE){
-                    //creating phoneCall record.
+
+               if(context.type == context.UserEventType.CREATE){
+
                     var phoneCall=record.create({
                         type:record.Type.PHONE_CALL
                     });
 
-                    phoneCall.setValue('title','Call HR for benefits');
-
-                    //list 그래서 internal id.
+                    phoneCall.setValue('title','Call HR for benefits.');
                     phoneCall.setValue('assigned', employee.id);
-
                     phoneCall.save();
                 }
+
+
            }
 
         };
